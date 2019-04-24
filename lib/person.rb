e5b84039e2d467f078aa3c5f1a9a2aaca92d0cc8
Katherine Wren <kwren@outlook.com>
2019-04-24 16:00:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -67,11 +67,8 @@ class Person
   end 
   
   def call_friend(person)
-    binding.pry
     person.happiness = @happiness + 3
-    binding.pry
     self.happiness = @happiness + 3
-    binding.pry
   return   "Hi #{person.name}! It's #{self.name}. How are you?"
   end 
   
