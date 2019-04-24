ba2294b9b8fa698006af16d9a52218fa322698e8
Katherine Wren <kwren@outlook.com>
2019-04-24 14:17:52 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ class Person
   def initialize(name)
     @name = name
     @bank_account = 25
-    @happiness_points = 8
+    @happiness = 8
     #[@happiness_points, 0, 10].sort[1]#
     @hygiene_points = 8
   end
