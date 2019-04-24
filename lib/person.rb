cd7daa003dd62ba0fea513e04d8ccb14bc64021c
Katherine Wren <kwren@outlook.com>
2019-04-24 15:18:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -75,9 +75,12 @@ class Person
   
   def start_conversation(person, topic)
     if topic = 'politics'
+      self.happiness = @happiness - 2
+      person.happiness = @happiness - 2
     return 'blah blah partisan blah lobbyist'
-  else
+else
   return 'blah blah blah blah blah'
+end
   end
   
 end
