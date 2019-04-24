241ea430a87bc6a12aa74bf58c41d100060adb36
Katherine Wren <kwren@outlook.com>
2019-04-24 14:16:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,10 @@ class Person
   
   def initialize(name)
     @name = name
+    @bank_account = 25
+    @happiness_points = 8
+    #[@happiness_points, 0, 10].sort[1]#
+    @hygiene_points = 8
   end
   
   attr_accessor :name
